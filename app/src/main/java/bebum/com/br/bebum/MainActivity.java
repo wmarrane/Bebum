b70112a672a7635be8ded97a05439cba81696fc7
addmn.cassio <cassio.menabue@gmail.com>
2017-05-05 23:07:24 -0300

    Inclusao variavel int teste2

--- app/src/main/java/bebum/com/br/bebum/MainActivity.java
+++ app/src/main/java/bebum/com/br/bebum/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private int teste;
+    private int teste2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
