f9abff4b203896c340e1b34e32edfca2b84d83fa
addmn.cassio <cassio.menabue@gmail.com>
2017-05-05 23:19:05 -0300

    Criacao de pacotes: ui util movendo arquivo MainActivity para o pacote ui

--- app/src/main/java/bebum/com/br/bebum/ui/MainActivity.java
+++ app/src/main/java/bebum/com/br/bebum/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package bebum.com.br.bebum;
+package bebum.com.br.bebum.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import bebum.com.br.bebum.R;
 
 public class MainActivity extends AppCompatActivity {
 
