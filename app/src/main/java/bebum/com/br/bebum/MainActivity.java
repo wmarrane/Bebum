65d0c8c900909c0f6a3d6a076f4d1916c3f46bf3
addmn.cassio <cassio.menabue@gmail.com>
2017-05-05 23:11:05 -0300

    Inclusao variavel TextView cassinho int teste05

--- app/src/main/java/bebum/com/br/bebum/MainActivity.java
+++ app/src/main/java/bebum/com/br/bebum/MainActivity.java
@@ -2,12 +2,14 @@ package bebum.com.br.bebum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int teste;
     private int teste2;
-
+    private TextView cassinho;
+    private int test5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
