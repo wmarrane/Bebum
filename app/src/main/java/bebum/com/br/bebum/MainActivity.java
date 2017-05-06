cf39d74f6ff306fd886d014a00df19d0f2a327a4
wmarr <wmarrane@gmail.com>
2017-05-05 22:52:41 -0300

    atualização no commit

--- app/src/main/java/bebum/com/br/bebum/MainActivity.java
+++ app/src/main/java/bebum/com/br/bebum/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int teste;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
